bbab782d46f315b15d0d22fe319c538b75530f50
seriy_p <165369835+BaseParsLib-dev@users.noreply.github.com>
2024-04-09 11:13:55 +1000

    Update lib version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='BaseParsLib',
-    version='0.0.3',
+    version='0.0.4',
     packages=['base_pars_lib', 'base_pars_lib.utils'],
     install_requires=[
         'fake-useragent==1.5.1',
