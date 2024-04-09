e811c7d9082f50a114295f37e46f9bcb34a12a83
seriy_p <165369835+BaseParsLib-dev@users.noreply.github.com>
2024-04-09 11:12:43 +1000

    Added logging

--- base_pars_lib/__init__.py
+++ base_pars_lib/__init__.py
@@ -1,2 +1,10 @@
+import logging
+
 from base_pars_lib.base_parser import BaseParser
 from base_pars_lib import utils
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(filename)s - func: %(funcName)s - message: %(message)s'
+)
