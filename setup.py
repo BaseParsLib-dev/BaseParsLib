1a774e6ef815533726cded2ddee93bf4d0a63935
seriy_p <165369835+BaseParsLib-dev@users.noreply.github.com>
2024-04-26 16:39:26 +1000

    Added config to packages

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ from setuptools import setup
 setup(
     name='BaseParsLib',
     version='0.1.2',
-    packages=['base_pars_lib', 'base_pars_lib.utils'],
+    packages=[
+        'base_pars_lib',
+        'base_pars_lib.utils',
+        'base_pars_lib.config'
+    ],
     install_requires=[
         'fake-useragent==1.5.1',
         'requests==2.31.0',
