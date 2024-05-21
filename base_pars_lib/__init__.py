e63298dd7b9424197fe3631787d5fcb85bb9438b
seriy_p <165369835+BaseParsLib-dev@users.noreply.github.com>
2024-05-21 11:40:56 +1000

    Allow AiohttpResponse to import

--- base_pars_lib/__init__.py
+++ base_pars_lib/__init__.py
@@ -1,3 +1,3 @@
-from base_pars_lib.async_base_parser import AsyncBaseParser
+from base_pars_lib.async_base_parser import AsyncBaseParser, AiohttpResponse
 from base_pars_lib.base_parser import BaseParser
 from base_pars_lib import utils
