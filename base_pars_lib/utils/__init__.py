d7f7a1f80177b89e7e673e01afe45830279c6763
seriy_p <165369835+BaseParsLib-dev@users.noreply.github.com>
2024-12-05 16:36:13 +0300

    Fixed imports

--- base_pars_lib/utils/__init__.py
+++ base_pars_lib/utils/__init__.py
@@ -1,5 +1,6 @@
 # ruff: noqa: F401
 
+from base_pars_lib.utils._parser_utils import get_data_from_text
 from base_pars_lib.utils._working_with_data import (
     split_on_chunks_by_chunk_len,
     split_on_chunks_by_count_chunks,
