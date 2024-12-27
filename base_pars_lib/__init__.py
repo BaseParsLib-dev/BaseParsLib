5a1915c353ad94c797ba2be4b7dfe78eaaed82f3
seriy_p <165369835+BaseParsLib-dev@users.noreply.github.com>
2024-12-27 13:25:21 +0300

    Update __init__.py

--- base_pars_lib/__init__.py
+++ base_pars_lib/__init__.py
@@ -5,3 +5,4 @@ from base_pars_lib.async_base_parser import AiohttpResponse, AsyncBaseParser
 from base_pars_lib.async_playwright_base_parser import AsyncPlaywrightBaseParser
 from base_pars_lib.base_parser import BaseParser
 from base_pars_lib.webdriver_base_parser import WebDriverBaseParser
+from base_pars_lib.async_nodriver_base_parser import AsyncNodriverBaseParser
