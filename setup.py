6abcc42f0b7bdfbf6cdac43fe02435cab820204d
seriy_p <165369835+BaseParsLib-dev@users.noreply.github.com>
2025-02-04 11:57:41 +0300

    Fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='BaseParsLib',
-    version='0.2.67',
+    version='0.2.68',
     packages=[
         'base_pars_lib',
         'base_pars_lib.utils',
