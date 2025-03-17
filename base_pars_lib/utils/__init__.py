fee5eba92ad1e2986e8045b3d561c2342ddb6965
seriy_p <165369835+BaseParsLib-dev@users.noreply.github.com>
2025-03-17 09:41:38 +0300

    Added import

--- base_pars_lib/utils/__init__.py
+++ base_pars_lib/utils/__init__.py
@@ -2,6 +2,7 @@
 
 from base_pars_lib.utils._parser_utils import get_data_from_text
 from base_pars_lib.utils._working_with_data import (
+    split_dict_on_chunks_by_chunk_len,
     split_on_chunks_by_chunk_len,
     split_on_chunks_by_count_chunks,
 )
