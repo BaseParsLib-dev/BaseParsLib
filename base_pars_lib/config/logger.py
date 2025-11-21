dc3331b1c63846d468f26f1c82229fee0b1a043e
seriy_p <seryi_pv@mail.ru>
2025-11-21 15:57:53 +0300

    Bug fix

--- base_pars_lib/config/logger.py
+++ base_pars_lib/config/logger.py
@@ -6,8 +6,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - me
 
 
 def backoff_exception(ex: Exception, iteration: int, print_logs: bool, url: str = "") -> None:
-    caller_frame = inspect.currentframe().f_back
-    func_name = caller_frame.f_code.co_name
+    func_name = __get_caller_name()
 
     if print_logs:
         print(f"{datetime.now()} - info - {func_name} - {ex}: iter {iteration}: url {url}")
@@ -16,8 +15,7 @@ def backoff_exception(ex: Exception, iteration: int, print_logs: bool, url: str 
 
 
 def backoff_status_code(status_code: int, iteration: int, url: str, print_logs: bool) -> None:
-    caller_frame = inspect.currentframe().f_back
-    func_name = caller_frame.f_code.co_name
+    func_name = __get_caller_name()
 
     if print_logs:
         print(
@@ -31,10 +29,25 @@ def backoff_status_code(status_code: int, iteration: int, url: str, print_logs: 
 
 
 def info_log(message: str, print_logs: bool) -> None:
-    caller_frame = inspect.currentframe().f_back
-    func_name = caller_frame.f_code.co_name
+    func_name = __get_caller_name()
 
     if print_logs:
         print(f"{datetime.now()} - info - {func_name} - {message}")
     else:
         logging.info(f"{func_name} - {message}")
+
+
+def __get_caller_name() -> str:
+    current_frame = inspect.currentframe()
+    if current_frame is None:
+        return ""
+
+    caller_frame = current_frame.f_back
+    if caller_frame is None:
+        return ""
+
+    caller_caller_frame = caller_frame.f_back
+    if caller_caller_frame is None:
+        return ""
+
+    return caller_caller_frame.f_code.co_name
